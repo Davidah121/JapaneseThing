cbc51f8cc951d9034305910dfa7224fe10288b32
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2019-03-03 14:13:18 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace JapaneseThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = UTF8Encoding.Unicode;
+            System.Console.InputEncoding = UTF8Encoding.Unicode;
+            
+
+            TestClass.addWordOrExpression("Translate into English", "こんにちは。", "Hello");
+            TestClass.addWordOrExpression("Translate into Japanese", "What time is it now.", "いまなんじですか。");
+            TestClass.addWordOrExpression("What is the question asking for", "せんこうはなにですか。", "What is your major");
+
+            TestClass.addWordOrExpression("What is the missing particle", "わたし＿なまえはあおばです。", "の");
+            TestClass.addWordOrExpression("What is 4:30 PM in japanese", "", "ごごよじはん。");
+            TestClass.addWordOrExpression("Answer in japanese", "あのう、すみません。せんこうはなにですか。", "The question is asking what your major is. For us, we would answer is こんぴゅーたーこうがくです。");
+
+            TestClass.addWordOrExpression("What is the missing particle. This conversation has two people talking.", "わたしのだいがくはじょーじあーさざん。ありすは？\nわたしのたいがく＿じょーじあーさざん。", "This conversation is about their shared college. Alice is the name of the second person. \nも is the correct answer");
+            TestClass.addWordOrExpression("Translate into japanese.", "Hello. My name is Murami. Nice to meet you.", "こんにちは。わたしはむらみです。どうぞよろしく。");
+
+
+            TestClass.beginTest();
+
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace JapaneseThing
+{
+    class TestClass
+    {
+        private static List<String> questions = new List<String>();
+        private static List<String> wordsOrExpressions = new List<String>();
+        private static List<String> translations = new List<String>();
+
+        public static void beginTest()
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Please Write your answers on a sheet of paper to practice writing.");
+            Console.WriteLine("Press Enter to get the answer. Press enter again to go to the next question.");
+            Console.WriteLine("This gives you enough time to compare your answer.\n");
+
+            //Record what question have been asked
+            List<int> questionsAsked = new List<int>();
+            int index = -1;
+
+            Stopwatch stopwatch = new Stopwatch();
+            for (int questionNumber=1; questionNumber < questions.Count+1; questionNumber++)
+            {
+                Console.WriteLine("Question " + questionNumber + ":");
+
+                do
+                {
+                    index = random.Next(0, questions.Count);
+                } while (questionsAsked.Contains(index));
+
+                questionsAsked.Add(index);
+
+                Console.WriteLine(questions[index]);
+                
+                Console.WriteLine(wordsOrExpressions[index]);
+
+                stopwatch.Reset();
+
+                stopwatch.Start();
+
+                String answer = Console.ReadLine();
+
+                stopwatch.Stop();
+
+                Console.WriteLine("The right answer is: " + translations[index]+"\n");
+                Console.WriteLine("Compare your answer.\n");
+                Console.WriteLine("Time to complete: " + (double)stopwatch.ElapsedMilliseconds/1000.0 + " Seconds.\n");
+
+                answer = Console.ReadLine();
+            }
+        }
+
+        public static void addWordOrExpression(String question, String Text, String value)
+        {
+            questions.Add(question);
+            wordsOrExpressions.Add(Text);
+            translations.Add(value);
+            /*
+            if (!wordsOrExpressions.Contains(Text))
+            {
+                questions.Add(question);
+                wordsOrExpressions.Add(Text);
+                translations.Add(value);
+            }
+            else
+            {
+                Console.WriteLine(Text + " is already in the list");
+            }
+            */
+        }
+    }
+}
